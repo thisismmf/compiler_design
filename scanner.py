1c014623109c9e5f46b09d697642a080c3efc81f
mfdevelop <mfhiphop390@gmail.com>
2023-03-29 17:27:23 +0330

    change scanner

--- scanner.py
+++ scanner.py
@@ -1,10 +1,14 @@
 class Scanner:
     def __init__(self, dfa, input_text=None, ):
+        self.scanning_line = 0
         self.dfa = dfa
         self.input_text = input_text
 
-    def panic_mode(self, char):
-        char = char
-
-    def get_next_token(self, char: str):
+    def panic_mode(self):
         pass
+
+    def get_next_token(self):
+        pass
+
+    def change_scanning_line(self):
+        self.scanning_line += 1
